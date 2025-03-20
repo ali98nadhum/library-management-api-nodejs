1b40a37c2550ccacae91ca8db990397a031a958e
Ali nadhum <alinadhum1998@gmail.com>
2025-03-21 01:49:59 +0300

    add login route

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,12 +1,15 @@
-const { register } = require("../controllers/AuthControler");
-const { registerValidator } = require("../utils/vaildators/AuthValidator");
+const { register, login } = require("../controllers/AuthControler");
+const { registerValidator, loginValidator } = require("../utils/vaildators/AuthValidator");
 
 const router = require("express").Router();
 
 
 
 router
-  .route("/register").post(registerValidator , register)
+.route("/register").post(registerValidator , register)
+
+
+router.route("/login").post(loginValidator , login)
 
 
   module.exports = router;
