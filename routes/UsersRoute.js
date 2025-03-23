9c8aec89d2606e5385f46305c88d520399f07b3e
Ali nadhum <alinadhum1998@gmail.com>
2025-03-23 19:14:53 +0300

    update getAllUsers route

--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -1,9 +1,9 @@
-
 const router = require("express").Router();
+const AuthService = require("../utils/token/AuthService");
 const { getAllUsers } = require("../controllers/UserController");
 
-
-router.route("/get-all-users").get(getAllUsers)
-
+router
+  .route("/get-all-users")
+  .get(AuthService.protect, AuthService.allowedTo("admin"), getAllUsers);
 
 module.exports = router;
