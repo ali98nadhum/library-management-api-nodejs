7803e034324718cf92d8aadc4b4d4770d44e6ea6
Ali nadhum <alinadhum1998@gmail.com>
2025-03-23 19:20:16 +0300

    add deleteUser route

--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const AuthService = require("../utils/token/AuthService");
-const { getAllUsers } = require("../controllers/UserController");
+const { getAllUsers, deleteUser } = require("../controllers/UserController");
 
 router
   .route("/get-all-users")
   .get(AuthService.protect, AuthService.allowedTo("admin"), getAllUsers);
+
+
+  router
+  .route("/delete-user/:id")
+  .delete(AuthService.protect,AuthService.allowedTo("admin"),deleteUser);
 
 module.exports = router;
