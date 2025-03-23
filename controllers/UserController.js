e78424c672ee7a2c1ffa3cf90a9f6eb5c37f21fa
Ali nadhum <alinadhum1998@gmail.com>
2025-03-23 19:18:38 +0300

    add deleteUser fun

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,3 +14,19 @@ module.exports.getAllUsers = asyncHandler(async(req , res) => {
     const users = await UserModel.find()
     res.status(200).json({data: users})
 })
+
+
+// ==================================
+// @desc Delete User
+// @route /api/v1/users/delete-user/:id
+// @method DELETE
+// @access private (admin)
+// ==================================
+module.exports.deleteUser = asyncHandler(async(req , res) => {
+    const user = await UserModel.findByIdAndDelete(req.params.id);
+    if(!user){
+        return res.status(404).json({message: "User not found for this id"})
+    }
+
+    res.status(200).json({message: "User deleted successfully"})
+})
