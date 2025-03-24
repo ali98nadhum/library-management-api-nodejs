478b58a8a3ebeca43443dd19a1f38ddeb6dc431d
Ali nadhum <alinadhum1998@gmail.com>
2025-03-24 21:31:51 +0300

    add createNewBook route

--- routes/BookRoute.js
+++ routes/BookRoute.js
@@ -1,10 +1,15 @@
-const { getAllBooks } = require("../controllers/BookController");
-
+const { getAllBooks, createNewBook } = require("../controllers/BookController");
+const AuthService = require("../utils/token/AuthService");
 const router = require("express").Router();
 
 router
   .route("/")
   .get(getAllBooks);
+
+
+router
+  .route("/add-book")
+  .post(AuthService.protect,AuthService.allowedTo("admin"),createNewBook)
 
 
 module.exports = router;
