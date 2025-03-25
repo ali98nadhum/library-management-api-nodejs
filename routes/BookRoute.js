c75e6e37445fe70c2d245f291bcd98bf6b6306c4
Ali nadhum <alinadhum1998@gmail.com>
2025-03-25 13:29:32 +0300

    update createBook route

--- routes/BookRoute.js
+++ routes/BookRoute.js
@@ -1,16 +1,19 @@
 const { getAllBooks, createNewBook } = require("../controllers/BookController");
+const { createBookValidator } = require("../utils/vaildators/bookValidator");
 const AuthService = require("../utils/token/AuthService");
 const uploadPhoto = require("../middlewares/configUploadImage");
 const router = require("express").Router();
 
-router
-  .route("/")
-  .get(getAllBooks);
-
+router.route("/").get(getAllBooks);
 
 router
   .route("/add-book")
-  .post(AuthService.protect,AuthService.allowedTo("admin"),uploadPhoto.single("image"),createNewBook)
-
+  .post(
+    AuthService.protect,
+    AuthService.allowedTo("admin"),
+    createBookValidator,
+    uploadPhoto.single("image"),
+    createNewBook
+  );
 
 module.exports = router;
