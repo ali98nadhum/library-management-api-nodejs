bfdc98ac2b251a49f2f27eaebf38680d0e3fa2ee
Ali nadhum <alinadhum1998@gmail.com>
2025-03-26 16:51:17 +0300

    add updateBookValidator

--- utils/vaildators/bookValidator.js
+++ utils/vaildators/bookValidator.js
@@ -71,3 +71,63 @@ exports.createBookValidator = [
     check("id").isMongoId().withMessage("Invalid book id"),
     VaildatorMiddleware,
   ];
+
+
+
+  exports.updateBookValidator = [
+    check("title")
+    .optional()
+    .isLength({ min: 3 })
+    .withMessage("book title must be at least 3 characters")
+    .isLength({ max: 100 })
+    .withMessage("book title should not exceed 100 characters"),
+
+    check("author")
+    .optional()
+    .isLength({ min: 3 })
+    .withMessage("author name must be at least 3 characters")
+    .isLength({ max: 50 })
+    .withMessage("author name should not exceed 50 characters"),
+
+    check("price")
+    .optional()
+    .isNumeric()
+    .withMessage("price must be a number")
+    .isInt({min: 0})
+    .withMessage("quantity must be 0 or greater"),
+
+    check("quantity")
+    .optional()
+    .isNumeric()
+    .withMessage("quantity must be a number")
+    .isInt({ min: 0 })
+    .withMessage("quantity must be 0 or greater")
+    .toInt(),
+
+    check("publishYear")
+    .optional()
+    .isNumeric()
+    .withMessage("publish year must be a number")
+    .isInt({ min: 0 })
+    .withMessage("You cannot put a negative number"),
+
+    check("publisher")
+    .optional()
+    .isLength({min: 3})
+    .withMessage("publisher name must be at least 3 characters")
+    .isLength({ max: 50 })
+    .withMessage("publisher name should not exceed 50 characters"),
+
+    check("category")
+    .optional()
+    .isMongoId()
+    .withMessage("Invalid category id"),
+
+    check("available")
+    .optional()
+    .isBoolean()
+    .withMessage("available must be a boolean value"),
+
+
+    VaildatorMiddleware,
+  ];
