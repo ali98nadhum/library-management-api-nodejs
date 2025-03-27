bcaa6598e483b8e5c361f0e49b24c8a0106b5d92
Ali nadhum <alinadhum1998@gmail.com>
2025-03-27 14:42:55 +0300

    add createOrder fun

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -13,3 +13,25 @@ module.exports.getAllOrders = asyncHandler(async(req, res) => {
     const orders = await OrderModel.find({})
     res.status(200).json({data: orders})
 })
+
+
+// ==================================
+// @desc Create a new Order
+// @route /api/v1/orders
+// @method POST
+// @access private (admin + employees)
+// ==================================
+module.exports.createOrder = asyncHandler(async(req, res) => {
+    const order = new OrderModel({
+        custmerName: req.body.custmerName,
+        totalPrice: req.body.totalPrice,
+        phone: req.body.phone,
+        address: req.body.address,
+        books: req.body.books,
+        user: req.body.user,
+    });
+
+    await order.save();
+
+    res.status(201).json({message: "تم انشاء الطلب بنجاح" , order: order})
+})
