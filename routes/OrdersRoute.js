fb3fb23274b09faeb908580d7cc09c89da9b54b5
Ali nadhum <alinadhum1998@gmail.com>
2025-03-27 14:44:42 +0300

    add createOrder route

--- routes/OrdersRoute.js
+++ routes/OrdersRoute.js
@@ -1,16 +1,14 @@
 
 
-const { getAllOrders } = require("../controllers/OrderController");
+const { getAllOrders, createOrder } = require("../controllers/OrderController");
 const AuthService = require("../utils/token/AuthService");
+const { createOrderValidator } = require("../utils/vaildators/orderValidator");
 const router = require("express").Router();
 
 router
 .route("/")
-.get(
-    AuthService.protect,
-    AuthService.allowedTo("admin" , "employee"),
-    getAllOrders
-);
+.get(AuthService.protect,AuthService.allowedTo("admin" , "employee"),getAllOrders)
+.post(createOrderValidator , createOrder)
 
 
 module.exports = router;
