3a75b24323b12f583fa5e0c5a563b0106758f9cd
Ali nadhum <alinadhum1998@gmail.com>
2025-03-28 14:41:48 +0300

    add getOrderByID route

--- routes/OrdersRoute.js
+++ routes/OrdersRoute.js
@@ -1,14 +1,18 @@
 
 
-const { getAllOrders, createOrder } = require("../controllers/OrderController");
+const { getAllOrders, createOrder, getOrderByID } = require("../controllers/OrderController");
 const AuthService = require("../utils/token/AuthService");
-const { createOrderValidator } = require("../utils/vaildators/orderValidator");
+const { createOrderValidator, getOrderByIdValidator } = require("../utils/vaildators/orderValidator");
 const router = require("express").Router();
 
 router
 .route("/")
 .get(AuthService.protect,AuthService.allowedTo("admin" , "employee"),getAllOrders)
 .post(createOrderValidator , createOrder)
+
+router
+.route(":id")
+.get(getOrderByIdValidator , getOrderByID)
 
 
 module.exports = router;
