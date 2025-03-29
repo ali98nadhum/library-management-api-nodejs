005b90a949387c64fbbbfc2aab713ca3b31eff84
Ali nadhum <alinadhum1998@gmail.com>
2025-03-29 17:26:50 +0300

    update orderValidtar fun

--- utils/vaildators/orderValidator.js
+++ utils/vaildators/orderValidator.js
@@ -2,9 +2,8 @@ const { check } = require("express-validator");
 const VaildatorMiddleware = require("../../middlewares/VaildatorMiddleware");
 const { OrderModel } = require("../../models/OrderModel");
 
-
 exports.createOrderValidator = [
-    check("custmerName")
+  check("custmerName")
     .notEmpty()
     .withMessage("customer name is required")
     .isLength({ min: 3 })
@@ -12,71 +11,64 @@ exports.createOrderValidator = [
     .isLength({ max: 50 })
     .withMessage("customer name should not exceed 50 characters"),
 
-   
+  check("phone").isMobilePhone("ar-IQ").withMessage("Invalid phone number"),
 
-    check("phone")
-    .isMobilePhone("ar-IQ")
-    .withMessage("Invalid phone number"),
-
-
-    check("address")
+  check("address")
     .notEmpty()
     .withMessage("address is required")
-    .isLength({min:10})
+    .isLength({ min: 10 })
     .withMessage("address must be at least 10 characters")
     .isLength({ max: 150 })
     .withMessage("address should not exceed 150 characters"),
 
-    check("books")
+  check("books")
     .notEmpty()
     .withMessage("books is required")
-    .isMongoId().withMessage("Invalid book id")
+    .isMongoId()
+    .withMessage("Invalid book id")
     .isArray(),
-    VaildatorMiddleware,
-  ];
+  VaildatorMiddleware,
+];
 
+exports.getOrderByIdValidator = [
+  check("id").isMongoId().withMessage("Invalid order id"),
+  VaildatorMiddleware,
+];
 
+exports.updateOrderValidator = [
+  check("id").isMongoId().withMessage("Invalid order id"),
 
-  exports.getOrderByIdValidator = [
-   check("id")
-   .isMongoId()
-   .withMessage("Invalid order id"),
-    VaildatorMiddleware,
-  ];
-
-
-
-  exports.updateOrderValidator = [
-    check("custmerName")
+  check("custmerName")
     .optional()
     .isLength({ min: 3 })
     .withMessage("customer name must be at least 3 characters")
     .isLength({ max: 50 })
     .withMessage("customer name should not exceed 50 characters"),
 
-
-    check("phone")
+  check("phone")
     .optional()
     .isMobilePhone("ar-IQ")
     .withMessage("Invalid phone number"),
 
-
-    check("address")
+  check("address")
     .optional()
-    .isLength({min:10})
+    .isLength({ min: 10 })
     .withMessage("address must be at least 10 characters")
     .isLength({ max: 150 })
     .withMessage("address should not exceed 150 characters"),
 
-   check("status")
-   .optional()
-   .isIn(["جاري التجهيز", "تم التجهيز" , "تم الالغاء"])
-   .withMessage("حاله الطلب يجب ان تكون 'جاري التجهيز او تم التجهيز او تم الالغاء"),
+  check("status")
+    .optional()
+    .isIn(["جاري التجهيز", "تم التجهيز", "تم الالغاء"])
+    .withMessage(
+      "حاله الطلب يجب ان تكون 'جاري التجهيز او تم التجهيز او تم الالغاء"
+    ),
 
-   check("deliveryStatus")
-   .optional()
-   .isIn(["جاري التوصيل" , "تم التوصيل" , "تم الالغاء"])
-   .withMessage("حاله التوصيل يجب ان تكون جاري التوصيل او تم التوصيل او تم الالغاء"),
-    VaildatorMiddleware,
-  ];
- 
+  check("deliveryStatus")
+    .optional()
+    .isIn(["جاري التوصيل", "تم التوصيل", "تم الالغاء"])
+    .withMessage(
+      "حاله التوصيل يجب ان تكون جاري التوصيل او تم التوصيل او تم الالغاء"
+    ),
+  VaildatorMiddleware,
+];
